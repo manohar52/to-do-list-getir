f5a561ab413fc4017ed1b4efbe81785936d59fda
Rajaram <mr086855@cerner.net>
2022-02-27 16:01:50 -0600

    index upd

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const corsOpts = {
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
